909f40d91ce93baadb1684c7afaa08c60dec25bf
Zh1m1ngC <169105236+Zh1m1ngC@users.noreply.github.com>
2024-05-06 18:56:55 -0400

    Add files via upload

---
+++ CS623_Project.py
@@ -0,0 +1,48 @@
+import psycopg2
+
+# Connect to PostgreSQL database
+conn = psycopg2.connect(
+    dbname="postgres",
+    user="postgres",
+    password="password",
+    host="localhost",
+    port="5432"
+)
+
+try:
+    # atomicity
+    conn.autocommit = False
+    # isolation
+    conn.set_isolation_level(3)
+    
+    # Create a cursor object
+    cur = conn.cursor()
+
+    # Add a new depot to the Depot table
+    cur.execute("""
+        INSERT INTO Depot (#dep, city, capacity)
+        VALUES ('d100', 'Chicago', 100)
+    """)
+
+    # Insert the new product into the Stock table for the newly added depot
+    cur.execute("""
+        INSERT INTO Stock (#prod, #dep, quantity)
+        VALUES ('p1', 'd100', 100)
+    """)
+
+    # Commit the transaction
+    conn.commit()
+
+except (Exception, psycopg2.DatabaseError) as e:
+    # Rollback the transaction if there is an error
+    conn.rollback()
+    print(f"Error: {e}")
+
+finally:
+    if conn:
+        # close the connection
+        cur.close()
+        conn.close()
+        print("PostgreSQL closed")
+
+print("End")
